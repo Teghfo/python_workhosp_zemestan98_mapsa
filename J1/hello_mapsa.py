3b7907f57a7ea92de02c7151b321945eb0390ae5
Teghfo <example@example.com>
2020-02-20 20:10:52 +0330

    init workshop

---
+++ J1/hello_mapsa.py
@@ -0,0 +1,55 @@
+# print('hello')
+
+
+# hasangholi = 5
+
+# print(hasangholi)
+
+
+# # hasangholi = 8.4
+# # print(type(hasangholi))
+
+# # print(hasangholi)
+# # ldksangvlbqapiw
+# # hasangholi = 'Gavesho dus dare'
+# # print(type(hasangholi))
+# print('ghable hasan')
+
+# print(hasangholi)
+
+# # hasangholi = "Sakineh ro dus dare"
+
+# name = input("Enter your name: ")
+
+# print("hello" , name)
+
+
+
+def print_name(name):
+    hello_name = 'hello ' + name
+    print(hello_name)
+
+
+
+# list
+
+my_list = [5, 'ashkan', 8.6]
+print(my_list[2])
+
+
+# tuple
+
+my_tuple = (5, 'ashkan', 8.6, 8.6)
+print(my_tuple)
+
+
+# set
+my_set = {5, 'ashkan', 8.6, 8.6}
+print(my_set)
+
+# dictionary
+
+my_dictionary = {'name': 'ashkan', 'age': 29}
+print(my_dictionary['age'])
+
+print_name('ashkan')
